2ec20b14e779e85d44905f9a3304724f9ce4540c
Yenkobe <yenduykr@gmail.com>
2018-06-16 14:35:33 +0900

    tipsroom done^^

--- app/src/main/java/com/example/yenduy/knu/TipsRoom.java
+++ app/src/main/java/com/example/yenduy/knu/TipsRoom.java
@@ -1,67 +1,70 @@
 package com.example.yenduy.knu;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class TipsRoom extends AppCompatActivity {
 
+    GridLayout mainGrid;
 
-    private ImageButton book1;
-    private ImageButton UNIVLabrary;
-    private ImageButton photocopy;
-    private ImageButton dvd;
-
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tips_room);
 
-        //-----------------------Community room button---------------//
-        book1 = (ImageButton) findViewById(R.id.image1);
-        book1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent open = new Intent(TipsRoom.this, BOOK1.class);
-                startActivity(open);
 
-            }
-        });
-        //------------------- tips button ------------//
-        UNIVLabrary = (ImageButton) findViewById(R.id.image2);
-        UNIVLabrary.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent open = new Intent(TipsRoom.this, UNIVlibrary.class);
-                startActivity(open);
-            }
-        });
+        mainGrid = (GridLayout)findViewById(R.id.mainGrid);
+        setSingleEvent(mainGrid);
 
-        //-----------------------------------------//
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        photocopy = (ImageButton) findViewById(R.id.image3);
-        photocopy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent open = new Intent(TipsRoom.this, UNIVphotocopy.class);
-                startActivity(open);
-            }
-        });
-
-        //-----------------------------------------//
-
-        dvd = (ImageButton) findViewById(R.id.image4);
-        dvd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent open = new Intent(TipsRoom.this, DVDroom.class);
-                startActivity(open);
-            }
-        });
-
-        //-----------------------------------------//
     }
+
+        private void setSingleEvent(GridLayout mainGrid) {
+            for (int i = 0; i  < mainGrid.getChildCount();i++){
+
+                CardView cardView = (CardView)mainGrid.getChildAt(i);
+                final int finalI = i;
+                cardView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        if (finalI == 0)  // open activity BOOK1
+                        {
+                            Intent intent = new Intent(TipsRoom.this, BOOK1.class);
+                            startActivity(intent);
+                        }
+
+                        else if (finalI ==1) // open activity dvd
+                        {
+                            Intent intent = new Intent(TipsRoom.this,DVDroom.class);
+                            startActivity(intent);
+                        }
+                        else if (finalI ==2) // open activity Comunity
+                        {
+                            Intent intent = new Intent(TipsRoom.this, UNIVphotocopy.class);
+                            startActivity(intent);
+                        }
+                        else if (finalI ==3) //  knu page
+                        {
+                            Intent intent = new Intent(TipsRoom.this, UNIVlibrary.class);
+                            startActivity(intent);
+                        }
+
+                    }
+                });
+
+            }
+        }
+
 }
