203f3a506080444599429957f9e25308103c69b3
Yenkobe <yenduykr@gmail.com>
2018-06-19 17:18:42 +0900

    update

--- app/src/main/java/com/example/yenduy/knu/CommunityRoom.java
+++ app/src/main/java/com/example/yenduy/knu/CommunityRoom.java
@@ -3,9 +3,11 @@ package com.example.yenduy.knu;
 import android.content.Context;
 
 import android.content.Intent;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,8 +16,16 @@ import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class CommunityRoom extends AppCompatActivity {
@@ -30,11 +40,49 @@ public class CommunityRoom extends AppCompatActivity {
         lvArticleList = (ListView) findViewById(R.id.lvArticleList);
 
         List articleList = new ArrayList();
+
+
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference ref = database.child("community");
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                //Post post = dataSnapshot.getValue(Post.class);
+                // ...
+
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    // TODO: handle the post
+
+
+                    for (DataSnapshot child : dataSnapshot.getChildren()){
+                        //child is each element in the finished list
+                        Map<String, String> message = (Map<String, String>)child.getValue();
+                        //message.get("date");
+                        Log.w("FIREBAS---", "EVENT" + message.get("date") );
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w("FIREBAS---", "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        };
+
+         ref.addValueEventListener(postListener);
+
+
         for (int i = 0; i < 300; i++) {
-            articleList.add(new ArticleVO("Item" + i, ".", new Random().nextInt(9999)));
+            articleList.add(new ArticleVO("Item" + i, ".", new Random().nextInt(999)));
         }
 
         lvArticleList.setAdapter(new ArticleListViewAdapter(articleList, this));
+
+
 
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -96,4 +144,3 @@ public class CommunityRoom extends AppCompatActivity {
     }
 
 }
-
--- app/src/main/java/com/example/yenduy/knu/CommunitySend.java
+++ app/src/main/java/com/example/yenduy/knu/CommunitySend.java
@@ -7,11 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CommunitySend extends AppCompatActivity {
 
 
-
+    private FirebaseAuth firebaseAuth;
 
 
     @Override
@@ -25,6 +39,25 @@ public class CommunitySend extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+
+                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference(); //Get Reference database
+                Map<String, String> sendData = new HashMap<>();
+
+
+                DateFormat df = new SimpleDateFormat("EEE, d MMM yyyy, HH:mm");
+                String date = df.format(Calendar.getInstance().getTime());
+
+
+                EditText editText = (EditText) findViewById(R.id.editText);
+
+                String msg = editText.getText().toString();
+                sendData.put("date", date );
+                sendData.put("text", msg );
+/*
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                sendData.put("email", user.getEmail() );
+*/
+                databaseReference.child("community").child(date).setValue(sendData);
 
                 //close activity
                 Intent intent = new Intent(CommunitySend.this, CommunityRoom.class );
